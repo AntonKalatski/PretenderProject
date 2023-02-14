2c76d7045e5d7c9e7f6c1341d6d61267adc192e9
AntonKalatski <kalatski24121991@gmail.com>
2023-02-14 16:52:37 +0300

    [master] Battle system initial

---
+++ Assets/Scripts/Game/CombatSystem/Targeting/Target.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Game.CombatSystem.Targeting
+{
+    public class Target : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Game/CombatSystem/Targeting/Targeter.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace Game.CombatSystem.Targeting
+{
+    public class Targeter : MonoBehaviour
+    {
+        [SerializeField] private HashSet<Target> _targets = new();
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.TryGetComponent(out Target target)) return;
+            _targets.Add(target);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (!other.TryGetComponent<Target>(out var target)) return;
+            if (_targets.Contains(target))
+                _targets.Remove(target);
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+        }
+    }
+}
